dd08199cdbf51f0c4d2b5017caad14b7454a02c6
Evstifeev Roman <someuniquename@gmail.com>
2020-02-15 18:53:21 +0300

    v0.7, add py 37, 38 to tox

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='websubsub',
-    version='0.6',
+    version='0.7',
     description='Django websub subscriber',
     long_description=description,
     author='Evstifeev Roman',
