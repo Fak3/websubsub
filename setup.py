a86f54daa13c7dc2d69fc33ef3cd2d9518476f9a
Evstifeev Roman <someuniquename@gmail.com>
2020-02-20 16:44:30 +0300

    v0.9.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='websubsub',
-    version='0.9.1',
+    version='0.9.2',
     description='Django websub subscriber',
     long_description=description,
     author='Evstifeev Roman',
